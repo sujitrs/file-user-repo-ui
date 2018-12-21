7c27debfd331fceeba1d734709f155bb879da02a
sujitrs <suryawanshisujeet@gmail.com>
2018-12-21 18:12:14 +0530

    working

---
+++ docs/4.0/examples/profile-file-upload/events.js
@@ -0,0 +1,103 @@
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+    document.getElementById('poiFile').addEventListener('change', processFile, false);
+    document.getElementById('poaFile').addEventListener('change', processFile, false);
+  
+    document.getElementById('downloadPoiFile').addEventListener('click', function(){downloadFile('poiFile')}, false);
+    document.getElementById('downloadPoaFile').addEventListener('click', function(){downloadFile('poaFile')}, false);
+  
+    document.getElementById('deletePoiFile').addEventListener('click', function(){deleteFile('poiFile')}, false);
+    document.getElementById('deletePoaFile').addEventListener('click', function(){deleteFile('poaFile')}, false);
+  } else {
+    alert('The File APIs are not fully supported in this browser.');
+  }
+  
+  
+  function downloadFile(file){
+    var fileField=document.getElementById(file).dataset.refid;
+    fetch('http://localhost:8181/getFile/'+fileField)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      var blob = b64toBlob(myJson.fileContent,myJson.fileType);
+      let objectURL = window.URL.createObjectURL(blob);
+      let anchor = document.createElement('a');
+      anchor.href = objectURL;
+      anchor.download = name;
+      anchor.click();
+      URL.revokeObjectURL(objectURL);
+    });
+  }
+  
+  function deleteFile(file){
+    var fileField=document.getElementById(file).dataset.refid;
+    fetch('http://localhost:8181/removeFile/'+fileField,{method: 'PATCH'}).then(function(response) {
+      return response.json();
+    }).then(function(myJson) {alert(myJson)});
+    
+  }
+  //image/jpeg;base64,
+  function processFile(){
+    var f = this.files[0]; // FileList object
+    var att=this.id;
+    var reader = new FileReader();
+  
+    // Closure to capture the file information.
+  
+    reader.onload = (function(theFile,myFile) {
+    return function(e) {
+    var base64Data = e.target.result;
+    //console.log(base64Data);
+    var metaDataIndex=base64Data.indexOf(",");
+    document.getElementById(myFile).dataset.base=base64Data;
+    fetch('http://localhost:8181/addFile', {
+            method: 'POST',
+            body: JSON.stringify({
+              fileContent: base64Data.substring(metaDataIndex+1),// Had to add 1 to get actual index
+              fileType:f.type
+            }),
+            headers: {
+              "Content-type": "application/json; charset=UTF-8"
+            }
+          })
+          .then(response => response.json())
+          .then(json => document.getElementById(myFile).dataset.refid=json);
+    };
+  
+    })(f,att);
+    reader.readAsDataURL(f);
+  }
+  
+  
+    
+      $(document).ready(function () {  
+               $("#Save").click(function () {  
+                 var filed=document.getElementById('poiFile');
+                console.log(filed);
+                   
+               });  
+           });  
+  
+           function b64toBlob(b64Data, contentType, sliceSize) {
+              contentType = contentType || '';
+              sliceSize = sliceSize || 512;
+  
+              var byteCharacters = atob(b64Data);
+              var byteArrays = [];
+  
+              for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+                var slice = byteCharacters.slice(offset, offset + sliceSize);
+  
+                var byteNumbers = new Array(slice.length);
+                for (var i = 0; i < slice.length; i++) {
+                  byteNumbers[i] = slice.charCodeAt(i);
+                }
+  
+                var byteArray = new Uint8Array(byteNumbers);
+  
+                byteArrays.push(byteArray);
+              }
+                
+              var blob = new Blob(byteArrays, {type: contentType});
+              return blob;
+            }
