4ca2d7910bbbff28cf5f716363c9e4e7c128c7df
sujitrs <suryawanshisujeet@gmail.com>
2018-12-28 19:09:44 +0530

    release given

---
+++ docs/4.0/examples/const.js
@@ -0,0 +1,4 @@
+const userRepoURL="http://localhost:9000";
+const appRepoURL="http://localhost:7171";
+const fileRepoURL="http://localhost:8000";
+//data = '[{"userRepoURL" : "http://localhost:9000", "appRepoURL" : "http://localhost:7171","fileRepoURL":"http://localhost:8000"}]';
